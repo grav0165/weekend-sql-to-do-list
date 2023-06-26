72cacac0b726a034d1c79cf26c2f4b5d9ab66e36
Aubrey Graves <grav0165@gmail.com>
2023-06-26 16:03:41 -0500

    Changing Pool PG back to basic one for grading homework.

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,20 +1,10 @@
 const pg = require('pg');
 
-let pool;
-
-if (process.env.DATABASE_URL) {
-  pool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  })
-} else {
-  pool = new pg.Pool({
+let pool = new pg.Pool({
     host: 'localhost',
     port: 5432,
     database: 'weekend-to-do-app'
   })
-}
+
 
 module.exports = pool;
